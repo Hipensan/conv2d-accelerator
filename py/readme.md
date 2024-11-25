e2786ffd97c686bf34eb88f7d9dd15f148e5a499
Nuneog <k04005@naver.com>
2024-11-25 17:13:07 +0900

    extract weights data w. python

---
+++ py/readme.md
@@ -0,0 +1,9 @@
+<h1> Readme </h1>
+<p> * Requirements </p>
+
+* yolov3-tiny.cfg<br>
+* yolov3-tiny.weights<br>
+
+<p> * How to do </p>
+
+* use read_weight.ipynb to generate weights in .npy or .JSON<br>
---
+++ py/wt_gen.py
@@ -0,0 +1,10 @@
+print(f"case(i_ci)")
+for ci in range(3):
+    print(f"{ci}: begin")
+    print(f"    case(i_co)")
+    for co in range(16):
+        
+        print(f"        {co}: rom <= 144'h")
+    print("     endcase")
+    print("end")
+print("endcase")
